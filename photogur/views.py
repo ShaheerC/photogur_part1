0885383076f57208aa711bdb1021a27f03d451dd
Shaheer Choudhury <shaheer.choudhury@gmail.com>
2019-07-31 23:20:11 -0400

    finished photogur part 4

--- photogur/views.py
+++ photogur/views.py
@@ -18,5 +18,14 @@ def picture_show(request, id):
 def picture_search(request):
     query = request.GET['query']
     search_results = Picture.objects.filter(artist=query)
-    context = {'pictures': search_results}
+    context = {'pictures': search_results, 'query': query}
     return render(request, 'search.html', context)
+
+def create_comment(request):
+    picture_id = request.POST['picture']
+    picture = Picture.objects.filter(id=picture_id)[0]
+    name = request.POST['name']
+    comment = request.POST['comment']
+    new_comment = Comment(name=name, message=comment, picture=picture)
+    new_comment.save()
+    return HttpResponseRedirect(f'/pictures/{picture_id}')
